6d5c46a9c8d68419cf38a47f70ad1d5ff5bb5503
quirogatdf <quirogatdf@gmail.com>
2025-04-01 14:31:52 -0300

    feat: add card component

--- src/app/pages/cake/cake.components.ts
+++ src/app/pages/cake/cake.components.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, Cake } from 'lucide-angular';
 @Component({
   selector: 'app-cake',
   standalone: true,
+  imports: [LucideAngularModule],
   templateUrl: './cake.component.html',
 })
-export class CakeComponent {}
+export class CakeComponent {
+  readonly Cake = Cake;
+}
